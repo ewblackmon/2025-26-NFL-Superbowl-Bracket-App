b9b14d09d88bffa1c05a045209d40ffba91816c9
Eric Blackmon <ewblackmon@gmail.com>
2026-01-14 10:56:14 -0800

    push the leaderboard down a few pixels to display the app header in the background.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version to v13 (or higher) to force the update
-const CACHE_NAME = 'nfl-bracket-v17';
+const CACHE_NAME = 'nfl-bracket-v18';
 const ASSETS = [
     './',
     './index.html',
