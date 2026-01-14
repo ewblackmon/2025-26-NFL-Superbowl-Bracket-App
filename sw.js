e38f48064ebc5fe3a1eb79d0ed19037accd834c6
Eric Blackmon <ewblackmon@gmail.com>
2026-01-14 10:16:29 -0800

    Adjust right side of leaderboard again to make it fit on narrow phones

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version to v13 (or higher) to force the update
-const CACHE_NAME = 'nfl-bracket-v14';
+const CACHE_NAME = 'nfl-bracket-v15';
 const ASSETS = [
     './',
     './index.html',
