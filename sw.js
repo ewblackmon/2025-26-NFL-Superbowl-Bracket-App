b8f8eb46482a01d831255110bff4ff053b60187e
Eric Blackmon <ewblackmon@gmail.com>
2026-01-14 09:43:01 -0800

    update sw.js to push the leaderboard style fix out

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version to v13 (or higher) to force the update
-const CACHE_NAME = 'nfl-bracket-v13';
+const CACHE_NAME = 'nfl-bracket-v14';
 const ASSETS = [
     './',
     './index.html',
