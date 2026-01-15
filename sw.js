ca575fa159cbe1acd35c492c8dae018f237e73cc
Eric Blackmon <ewblackmon@gmail.com>
2026-01-14 22:04:36 -0800

    Forcing space between the place and the name

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version to v13 (or higher) to force the update
-const CACHE_NAME = 'nfl-bracket-v19';
+const CACHE_NAME = 'nfl-bracket-v20';
 const ASSETS = [
     './',
     './index.html',
