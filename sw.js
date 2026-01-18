641a191521c3c0491a1b08aeb34bdb08c56235fd
Eric Blackmon <ewblackmon@gmail.com>
2026-01-17 19:21:39 -0800

    Fix leaderboard height.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version to v13 (or higher) to force the update
-const CACHE_NAME = 'nfl-bracket-v21';
+const CACHE_NAME = 'nfl-bracket-v22';
 const ASSETS = [
     './',
     './index.html',
